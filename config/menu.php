d586d6fe7126961355663d7e53c5be3847ad7d02
uttkarsh1012 <64450571+uttkarsh1012@users.noreply.github.com>
2025-11-27 00:39:01 -0700

    Update menu.php

--- config/menu.php
+++ config/menu.php
@@ -1,3 +1,10 @@
-<?php 
-$module_menu = array();
+<?php
+// nobeds-channel-manager/config/menu.php
 
+$module_menu = array(
+    array(
+        'label'    => 'NoBeds Channel Manager',
+        'location' => 'PRIMARY',           // PRIMARY = left main menu
+        'link'     => 'nobeds_settings'    // Must match a route we’ll define
+    )
+);
