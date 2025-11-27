e27a93dc2a784a56c9083799b593e74542d35d86
uttkarsh1012 <64450571+uttkarsh1012@users.noreply.github.com>
2025-11-27 00:39:27 -0700

    Update route.php

--- config/route.php
+++ config/route.php
@@ -1,25 +1,11 @@
 <?php
+// nobeds-channel-manager/config/route.php
 
-//sample page
-$extension_route['sample_page'] = 'sample_controller/index';
+// Settings page
+$extension_route['nobeds_settings'] = 'Nobeds_controller/index';
 
-//list route    
-$extension_route['bookings_list'] = 'show_booking/show_latest_bookings';
-$extension_route['customer_list'] = 'show_booking/show_customer_list';
+// Manual sync (triggered by button)
+$extension_route['nobeds_manual_sync'] = 'Nobeds_controller/manual_sync';
 
-//post route
-$extension_route['post_data'] = 'custom_data/get_post_data';
-$extension_route['add_custom_data'] = 'custom_data/add_custom_data';
-$extension_route['edit_custom_data/(:any)'] = 'custom_data/edit_custom_data/$1';
-$extension_route['update_custom_data'] = 'custom_data/update_custom_data';
-$extension_route['delete_custom_data/(:any)'] = 'custom_data/delete_custom_data/$1';
-
-//option route
-$extension_route['option_data'] = 'option_data/get_option_data';
-$extension_route['add_option_data'] = 'option_data/add_option_data';
-$extension_route['edit_option_data'] = 'option_data/edit_option_data';
-$extension_route['update_option_data'] = 'option_data/update_option_data';
-$extension_route['delete_option_data'] = 'option_data/delete_option_data';
-
-//info route
-$extension_route['info_page'] = 'sample_controller/show_info_page';
+// Cron endpoint (for automatic sync)
+$extension_route['nobeds_cron_pull_bookings'] = 'Nobeds_cron/pull_bookings';
